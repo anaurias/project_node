1ea53e70c0d72ea81c7b3678681edf129194ff17
anaurias <anacarolina13a@gmail.com>
2019-10-13 16:51:37 -0300

    teste de inserção de user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/model/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Ana' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Ana',
+    email: 'anauriasmachado@gmail.com',
+    password_hash: '123465465',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
